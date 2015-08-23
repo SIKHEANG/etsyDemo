c9174b81fce2055f1b2e38e7fde9032e212640db
sikheang <ung.sikheang@gmail.com>
2015-08-24 00:03:25 +0700

    Added name to User

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap-sass', '~> 3.3.5'
 gem 'paperclip', '~> 4.3.0'
 gem 'paperclip-dropbox', '~> 1.3.2'
 gem 'figaro', '~> 1.1.1'
+gem 'devise', '~> 3.5.2'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,10 +3,11 @@ class Listing < ActiveRecord::Base
     has_attached_file :image, :styles => {
                       :medium => "200x200>", :thumb => "100x100>" },
                       :default_url => "default.png"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :image, :styles => {
                       :medium => "200x200>", :thumb => "100x100>" },
-                      :default_url => "default.png",
+                      :default_url => "/images/:style/default.png",
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
